50e4fdbc37f970ccda809596c50af4eb0fc7e0f9
crasm <crasm@git.email.vczf.io>
2017-06-09 16:59:20 -0400

    Make canvas draggable

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
 (function() {
+  var canvas = document.getElementById('c');
+  canvas.ondrag = function(e) {
+    console.log('ondrag: ' + e.screenX + ',' + e.screenY);
+  };
+  canvas.ondragstart = function(e) {
+    console.log('ondragstart: ' + e.screenX + ',' + e.screenY);
+  };
+  canvas.ondragend = function(e) {
+    console.log('ondragend: ' + e.screenX + ',' + e.screenY);
+  };
+
   function draw(img) {
-    var c = document.getElementById('c');
-    c.width = window.innerWidth;
-    c.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-    var ctx = c.getContext('2d');
+    var ctx = canvas.getContext('2d');
 
-    var cRatio = c.width / c.height;
+    var cRatio = canvas.width / canvas.height;
     var imgRatio = img.width / img.height;
 
-    console.log('c: ' + c.width + ', ' + c.height);
+    console.log('c: ' + canvas.width + ', ' + canvas.height);
     console.log('img: ' + img.width + ', ' + img.height);
 
     var same = cRatio == imgRatio;
     if (same) {
       console.log('same');
-      ctx.drawImage(img, 0, 0, c.width, c.height);
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
       return;
     }
 
     var skinny = imgRatio < cRatio;
     if (skinny) {
       console.log('skinny');
-      var dWidth = c.height * imgRatio;
-      var dHeight = c.height;
-      var dx = (c.width - dWidth) / 2;
+      var dw = canvas.height * imgRatio;
+      var dh = canvas.height;
+      var dx = (canvas.width - dw) / 2;
       var dy = 0;
       console.log('dx=' + dx + ',dy=' + dy);
-      ctx.drawImage(img, dx, dy, dWidth, dHeight);
+      ctx.drawImage(img, dx, dy, dw, dh);
       return;
     }
 
     var fat = cRatio < imgRatio;
     if (fat) {
       console.log('fat');
-      var dWidth = c.width;
-      var dHeight = c.width / imgRatio;
+      var dw = canvas.width;
+      var dh = canvas.width / imgRatio;
       var dx = 0;
-      var dy = (c.height - dHeight) / 2;
+      var dy = (canvas.height - dh) / 2;
       console.log('dx=' + dx + ',dy=' + dy);
-      ctx.drawImage(img, dx, dy, dWidth, dHeight);
+      ctx.drawImage(img, dx, dy, dw, dh);
       return;
     }
   }
